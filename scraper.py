9ded87e924849d31204d3199bcbe679f01146075
blablupcom <o.volodin@yahoo.com>
2015-06-10 19:30:58 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -239,10 +239,7 @@ if __name__ == '__main__':
                 attach_list = []
                 attach_list = get_attachments(tender_soup)
                 scraperwiki.sqlite.save(unique_keys=['l'], data={"l":unicode(link), "tender_id": unicode(tender_id), "buyer": unicode(buyer), "title" : unicode(title), "categories": unicode(categories), "contact_name": unicode(contact_name), "contact_phone": unicode(contact_phone), "contact_addr": unicode(contact_addr), "contact_email": unicode(contact_email), "contract_start": contract_start, "contract_end": contract_end, "eoi_start": eoi_start, "eoi_end": eoi_end, "est_value": unicode(est_value), "contract_duration": unicode(contract_duration), "extension_duration": unicode(extension_duration), "extension_iterations": unicode(extension_iterations), "summary": unicode(summary), "attach_list": unicode(attach_list),"d": todays_date })
-                # "tender_id": unicode(tender_id).encode('ascii', 'ignore').decode('utf-8', 'ignore'), "buyer": unicode(buyer).encode('ascii', 'ignore').decode('utf-8', 'ignore'), "title" : unicode(title).encode('ascii', 'ignore').decode('utf-8', 'ignore'), "categories": unicode(categories).encode('ascii', 'ignore').decode('utf-8', 'ignore'),  "d": todays_date.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-                # "contact_name": unicode(contact_name), "contact_phone": contact_phone,  "contact_addr": contact_addr.decode('utf-8'),  "contract_start": contract_start, "contract_end": contract_end, "eoi_start": eoi_start, "eoi_end": eoi_end, "est_value": est_value, "contract_duration": contract_duration.decode('utf-8'),"extension_duration": extension_duration.decode('utf-8'), "extension_iterations": extension_iterations.decode('utf-8'), "attach_list": unicode(attach_list),
-                # "contact_email": unicode(contact_email),
-                # "summary": summary.decode('utf-8')
+               
                 # csv_row = [link, tender_id,buyer,title,summary,categories,contact_name,contact_phone,contact_email,contact_addr,contract_start,contract_end,eoi_start,eoi_end,est_value,contract_duration,extension_duration,extension_iterations,attach_list]
                 #
                 # wr = csv.writer(resultFile, quoting=csv.QUOTE_ALL, delimiter=',')
